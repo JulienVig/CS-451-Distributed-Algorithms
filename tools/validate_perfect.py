f51cd45bd11513eb6b095265308a905c55425c70
Julien Vignoud <julien.vignoud@epfl.ch>
2021-11-02 18:09:54 +0100

    Improve validate_perfect

--- tools/validate_perfect.py
+++ tools/validate_perfect.py
@@ -30,7 +30,7 @@ def parse_output_files(sender_files, receiver_file):
             messages.append(m)
             unique_messages_received.add(m)
     print("Number of messages:", len(messages))
-    print("Number of duplicated delivered messages:", len(messages) - len(set(messages)))
+    # print("Number of duplicated delivered messages:", len(messages) - len(set(messages)))
     unique_messages_sent = set()
     for sender_file in sender_files:
         end_idx = sender_file.index('.output')
@@ -48,12 +48,12 @@ def parse_output_files(sender_files, receiver_file):
         missing_messages = {}
         for m in messages:
             sender_id = int(m.split(" ")[0])
-            seq_nb = int(m.plist(" ")[1])
+            seq_nb = int(m.split(" ")[1])
             if sender_id not in missing_messages:
                 missing_messages[sender_id] = []
             missing_messages[sender_id].append(seq_nb)
         for sender_id in missing_messages.keys():
-            print("Number of duplicated sent messages:", len(missing_messages[sender_id]) - len(set(missing_messages[sender_id])))
+            # print("Number of duplicated sent messages:", len(missing_messages[sender_id]) - len(set(missing_messages[sender_id])))
             print(f"{sender_id} has {len(missing_messages[sender_id])} packets not delivered by the receiver")
                 
 
